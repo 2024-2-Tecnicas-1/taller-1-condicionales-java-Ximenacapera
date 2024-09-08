0705dc5a3c40d214c65764d167d19f023c623ebb
Ximena Capera <ximenacapera79@gmail.com>
2024-09-07 23:18:47 -0500

    IMC

--- src/main/java/ejercicios/IMC.java
+++ src/main/java/ejercicios/IMC.java
@@ -1,22 +1,35 @@
 
 package ejercicios;
 
-import static ejercicios.SetDeTenis.evaluar;
+
 import java.util.Scanner;
 
-/**
- *
- * @author danielsanchez
- */
+
 public class IMC {
-    public static String evaluar(int peso, double estatura, int edad) {
-        // TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-        return "";
+
+     public static String evaluar(int peso, double estatura, int edad) {
+
+        double imc = peso / (estatura * estatura);
+        
+        // Determinar la condición de riesgo
+        if (edad > 45) {
+            if (imc < 22.0) {
+                return "Bajo peso";
+            } else {
+                return "Medio";
+            }
+        } else {
+            if (imc < 22.0) {
+                return "Medio";
+            } else {
+                return "Alto peso";
+            }
+        }
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-            System.out.print("Peso:");
+        System.out.print("Peso:");
         int peso = lector.nextInt();
         System.out.print("Estatura:");
         double estatura = lector.nextDouble();
