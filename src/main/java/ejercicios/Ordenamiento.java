0ea0cd6800058eec4f4f14b39dacf62adc96e6fb
Ximena Capera <ximenacapera79@gmail.com>
2024-09-07 22:50:29 -0500

    ordenar

--- src/main/java/ejercicios/Ordenamiento.java
+++ src/main/java/ejercicios/Ordenamiento.java
@@ -1,7 +1,7 @@
 
 package ejercicios;
 
-import static ejercicios.SetDeTenis.evaluar;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,10 +10,24 @@ import java.util.Scanner;
  */
 public class Ordenamiento {
     public static String evaluar(int numero1, int numero2, int numero3, int numero4) {
-        // TODO: Coloca aquí el código del ejercicio 5: Ordenamiento
-        return "";
+       char[] numeros = {
+            (char) (numero1 + '0'), // Convertir int a char representando el dígito
+            (char) (numero2 + '0'),
+            (char) (numero3 + '0'),
+            (char) (numero4 + '0')
+        };
+        
+        // Ordenar el array de caracteres
+        Arrays.sort(numeros);
+        
+        // Convertir caracteres de vuelta a números y construir la respuesta
+        StringBuilder respuesta = new StringBuilder("Números ordenados de menor a mayor:\n");
+        for (char c : numeros) {
+            respuesta.append(c - '0').append("\n"); // Convertir char a int
+        }
+        
+        return respuesta.toString();
     }
-    
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Número 1:");
