d265947f4d86302fea1dbb6d32d2728368288435
Ximena Capera <ximenacapera79@gmail.com>
2024-09-07 23:08:12 -0500

    Tennis

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -2,14 +2,41 @@ package ejercicios;
 
 import java.util.Scanner;
 
-/**
- *
- * @author danielsanchez
- */
+
 public class SetDeTenis {
     public static String evaluar(int numVictoriasA, int numVictoriasB) {
-        // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-        return "";
+        if ((numVictoriasA < 0 || numVictoriasB < 0) || (numVictoriasA > 7 || numVictoriasB > 7) ||
+            (numVictoriasA > 6 && numVictoriasB > 6) || (numVictoriasA == 7 && numVictoriasB == 7)) {
+            return "Inválido";
+        }
+        
+        
+        if (numVictoriasA >= 6 && numVictoriasA - numVictoriasB >= 2) {
+            return "Ganó A";
+        } else if (numVictoriasB >= 6 && numVictoriasB - numVictoriasA >= 2) {
+            return "Ganó B";
+        }
+        
+    
+        if (numVictoriasA == 5 && numVictoriasB == 5) {
+            if (numVictoriasA == 7) {
+                return "A ganó el set";
+            } else if (numVictoriasB == 7) {
+                return "Ganó B";
+            }
+        }
+        
+
+        if (numVictoriasA == 6 && numVictoriasB == 6) {
+            if (numVictoriasA == 7 && numVictoriasB == 6) {
+                return "Ganó A";
+            } else if (numVictoriasB == 7 && numVictoriasA == 6) {
+                return "B ganó el set";
+            }
+        }
+        
+    
+        return "Aún  no termina";
     }
     
     public static void main(String[] args) {
